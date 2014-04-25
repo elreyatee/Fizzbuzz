2d0885db32efaa86d43920463861617a341c9801
Ellery Temple Jr <elreyatee@gmail.com>
2014-04-25 06:11:50 -0700

    Updated fizz buzz
    Added prompt for number = limit for fizz buzz loop
    Check if it’s NaN and decimal
    Added ability to keep running fizz buzz and remove all child elements

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
 
+	function startFB () {
+		var number = prompt("Please enter a number");
+		while (isNaN(+number) == true) {
+			number = prompt("Supply a new number");
+		} 
+		return +number;
+	}
+	
 	function createDiv (value) {
-		$('<div><p>' + value + '</p></div>').appendTo('body');
+		$('<div class="style"><p>' + value + '</p></div>').appendTo('#wrapper');
 	}
 
 	$('body').on('click', 'button', function() {
 
-		for(var i = 1; i <= 100; i++) {
+		$('#wrapper').empty();
+		var limit = Math.floor(startFB());
+
+		for(var i = 1; i <= limit; i++) {
 			if(i % 15 == 0) {
 				createDiv("fizz buzz");
 			} else if (i % 3 == 0) {
